7c3e1fb623f4600d82a661ca6884188860da4dd2
Aerish <aryalaerish369@gmail.com>
2024-05-25 18:51:00 +0545

    fixed frontend

--- auctions/urls.py
+++ auctions/urls.py
@@ -8,9 +8,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("register/", views.register, name="register"),
 
     #! Crud of the listings
     path('create-listing', views.createListing, name='create-listing'),
